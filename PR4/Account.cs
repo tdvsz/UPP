ff932007f3d91a5c147eded7d826a1dd5fe0a959
tdvsz <avtuhmaxim@yandex.by>
2025-03-24 15:40:03 +0300

    PR4

---
+++ PR4/Account.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upp
+{
+    class Account
+    {
+        private decimal Amount { get; set; }
+        private string Currency {  get; set; }
+
+        public Account(decimal amount, string currency)
+        {
+            Amount = amount;
+            Currency = currency;
+        }
+
+        public static Account AddAccount()
+        {
+            Console.Write("Введите сумму счета: ");
+            decimal amount = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Введите валюту счета: ");
+            string currency = Console.ReadLine();
+
+            return new Account(amount, currency); 
+        }
+
+        public void GetAmount() 
+        {
+            Console.WriteLine($"Сумма: {Amount}");
+        }
+        
+        public decimal ChangeAmount(decimal newAmount) {
+            if (newAmount > 0)
+            {
+                return Amount = newAmount;
+            }
+
+            else {
+                Console.WriteLine("Ошибка. Невозможно изменить счет");
+                return 0;
+            }
+        }
+
+        public string GetCurrency()
+        {
+            return Currency;
+        }
+
+        public string ChangeCurrency(string newCurrency)
+        {
+            bool validCurrency = false;
+            string[] currencyList = { "USD", "BYN", "EUR" };
+
+            for (int i = 0; i < currencyList.Length; i++) { 
+                if (currencyList[i] == newCurrency)
+                {
+                    validCurrency = true;
+                    break;
+                }
+            }
+
+            if (validCurrency)
+            {
+                return Currency = newCurrency;
+            }
+
+            else
+            {
+                Console.WriteLine("Ошибка. Неверная валюта");
+                return null;
+            }
+        }
+
+        public void AccountPrint()
+        {
+            Console.WriteLine($"Сумма счета: {Amount} {Currency}");
+        }
+
+        public static Account operator +(Account account1, Account account2)
+        {
+            if (account1.Currency != account2.Currency)
+            {
+                throw new InvalidOperationException("Валюты счетов не совпадают");
+            }
+
+            return new Account(account1.Amount + account2.Amount, account1.Currency);
+        }
+
+        public static Account operator -(Account account, decimal amount)
+        {
+            if (amount > 0 && account.Amount >= amount)
+            {
+                account.Amount -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка. На счете недостаточно средств.");
+            }
+
+            return account;
+        }
+
+        public static Account operator +(Account account, decimal amount)
+        {
+            if (amount > 0)
+            {
+                account.Amount += amount;
+            }
+
+            return account;
+        }
+
+        public static bool operator >(Account account1, Account account2)
+        {
+            if (account1.Currency == account2.Currency)
+            {
+                return account1.Amount > account2.Amount;
+            }
+            else
+            {
+                throw new InvalidOperationException("Валюты счетов не совпадают");
+            }
+        }
+
+        public static bool operator <(Account account1, Account account2)
+        {
+            if (account1.Currency == account2.Currency)
+            {
+                return account1.Amount < account2.Amount;
+            }
+            else
+            {
+                throw new InvalidOperationException("Валюты счетов не совпадают");
+            }
+        }
+
+        public static Account operator ++(Account account)
+        {
+            decimal interestRate = 0.05m;
+            account.Amount += account.Amount * interestRate;
+            return account;
+        }
+    }
+}
---
+++ PR4/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+
+namespace upp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = Account.AddAccount();
+            Account a2 = Account.AddAccount();
+            Account a3;
+
+            Console.WriteLine("Вывод счетов");
+            a1.AccountPrint();
+            a2.AccountPrint();
+
+            Console.WriteLine("\nСлияние счетов с разными валютами");
+            try
+            {
+                a3 = a1 + a2;
+                a3.AccountPrint();
+            }
+
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("\nСлияние счетов с одинаковыми валютами");
+            string c = a1.GetCurrency();
+            a2.ChangeCurrency(c);
+            a3 = a1 + a2;
+            a3.AccountPrint();
+
+            Console.WriteLine("\nСнятие со счета значения 5");
+            Console.Write("До: "); a1.AccountPrint();
+            a1 -= 5m;
+            Console.Write("После: "); a1.AccountPrint();
+
+            Console.WriteLine("\nПополнение счета значением 5");
+            Console.Write("До: "); a2.AccountPrint();
+            a2 += 5m;
+            Console.Write("После: "); a2.AccountPrint();
+
+            Console.WriteLine("\nСравниване счетов с одинаковыми валютами");
+            if (a1 > a2)
+            {
+                Console.WriteLine("Первый счет больше");
+            }
+            else
+            {
+                Console.WriteLine("Второй счет больше");
+            }
+
+            Console.WriteLine("\nСравниване счетов с разными валютами");
+            a1.ChangeCurrency("EUR");
+            try
+            {
+                if (a1 > a2)
+                {
+                    Console.WriteLine("Первый счет больше");
+                }
+                else
+                {
+                    Console.WriteLine("Второй счет больше");
+                }
+            }
+
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("\nНачисление процентов на текущую сумму на счете");
+            Console.Write("До: "); a1.AccountPrint();
+            a1++;
+            Console.Write("После: "); a1.AccountPrint();
+        }
+    }
+}
