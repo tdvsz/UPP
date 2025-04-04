d090d90d4c7d2cda656a26810e7729132ee50bbe
tdvsz <avtuhmaxim@yandex.by>
2025-04-04 10:51:29 +0300

    PR14

---
+++ PR14/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace upp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void DisplayMessage(string message) => Console.WriteLine(message);
+
+            Student student = new Student("Широкородцевич", "Сергей");
+            student.Notify += DisplayMessage;
+            student.AddMark(9);
+            student.AddMark(10);
+            student.AddMark(8);
+            student.Print();
+
+            Console.WriteLine();
+
+            Bachelor bachelor = new Bachelor("Балтрук", "Алексей");
+            bachelor.Notify += DisplayMessage;
+            bachelor.AddMark(11);
+            bachelor.AddMark(10);
+            bachelor.AddMark(8);
+            bachelor.Print();
+
+            Console.WriteLine();
+
+            Master master = new Master("Стоминок", "Никита");
+            master.Notify += DisplayMessage;
+            master.AddMark(10);
+            master.AddMark(10);
+            master.AddMark(9);
+            master.Print();
+        }
+    }
+}
---
+++ PR14/Student.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace upp
+{
+    public interface IMarks
+    {
+        void AddMark(int mark);
+    }
+
+    public interface IStudentInfo
+    {
+        void Print();
+    }
+
+    public class Student : IMarks, IStudentInfo
+    {
+        public delegate void StudentHandler(string message);
+        public event StudentHandler? Notify;
+
+        private List<int> Marks = new List<int>();
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public Student(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public void AddMark(int mark)
+        {
+            if (mark < 1 || mark > 10)
+            {
+                Notify?.Invoke($"Недопустимая оценка");
+                return;
+            }
+
+            Marks.Add(mark);
+            Notify?.Invoke($"Оценка добавлена {mark}");
+        }
+
+        public double GetAverageMark()
+        {
+            return Marks.Count == 0 ? 0 : Marks.Average();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Name} {Surname}. Средний балл: {GetAverageMark():F1}");
+        }
+    }
+
+    public class Bachelor : Student
+    {
+        public Bachelor(string name, string surname) : base(name, surname)
+        {
+
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Name} {Surname}. Средний балл: {GetAverageMark():F1}");
+        }
+    }
+
+    public class Master : Bachelor
+    {
+        public Master(string name, string surname) : base(name, surname)
+        {
+
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Name} {Surname}. Средний балл: {GetAverageMark():F1}");
+        }
+    }
+}
