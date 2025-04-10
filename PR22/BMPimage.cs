cebffb0611ed256754d3f4b0aea4473e4935bd15
tdvsz <avtuhmaxim@yandex.by>
2025-04-10 12:44:29 +0300

    PR22

---
+++ PR22/BMPimage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+
+namespace upp
+{
+    class BMPimage
+    {
+        private string reg = @".+_[\d]+[×x][\d]+";
+        private string Name { get; set; }
+        private int Width { get; set; }
+        private int Height { get; set; }
+
+        public BMPimage(string name) {
+            string[] fullName = name.Split('\\');
+            Name = fullName[fullName.Length - 1];
+
+            if (Regex.IsMatch(Name, reg))
+            {
+                char[] separatingStrings = { '_', 'x', '×', '.' };
+                string[] parts = Name.Split(separatingStrings);
+
+                Width = int.Parse(parts[1]);
+                Height = int.Parse(parts[2]);
+            }
+
+            else
+            {
+                Image img = Image.FromFile(name);
+                Width = img.Width;
+                Height = img.Height;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Width}x{Height}";
+        }
+    }
+}
---
+++ PR22/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace upp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo("C:/Users/User/Desktop/test");
+            List<BMPimage> bMPimages = new List<BMPimage>();
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                bMPimages.Add(new BMPimage(file.FullName));
+            }
+
+            foreach (BMPimage image in bMPimages) { 
+                Console.WriteLine(image);
+            }
+        }
+    }
+}
